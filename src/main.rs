c422eaea2c9514fa2c1e7454407c0e2358fb1d39
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2023-01-17 00:42:23 +0100

    Benchmark addition

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,17 @@ use matrix::Vec2DMatrix;
 fn main() {
     let a: Vec2DMatrix<f32> = Vec2DMatrix::new_rand((4, 5));
     println!("{}", a);
+
+    let a: Vec2DMatrix<f32> = Vec2DMatrix::new_rand((10_000, 10_000));
+    let b: Vec2DMatrix<f32> = Vec2DMatrix::new_rand((10_000, 10_000));
+
+    use std::time::Instant;
+    let now = Instant::now();
+
+    {
+        let _ = a + b;
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -2,35 +2,64 @@ use rand::distributions::Standard;
 use rand::prelude::*;
 use rand::Rng;
 use std::fmt;
+use std::ops;
 
-pub struct Vec2DMatrix<T> {
+pub struct Vec2DMatrix<T: Copy> {
     data: Vec<Vec<T>>,
+    shape: (u32, u32),
 }
 
 impl<T> Vec2DMatrix<T>
 where
     Standard: Distribution<T>,
+    T: Copy,
 {
-    pub fn new_rand(size: (u8, u8)) -> Vec2DMatrix<T> {
+    pub fn new_rand(shape: (u32, u32)) -> Vec2DMatrix<T> {
         let mut rng = rand::thread_rng();
         let mut data = Vec::new();
 
-        for _ in 0..size.0 {
+        for _ in 0..shape.0 {
             data.push(Vec::new());
         }
 
         for sub_vec in &mut data {
-            for _ in 0..size.1 {
+            for _ in 0..shape.1 {
                 sub_vec.push(rng.gen::<T>());
             }
         }
 
-        Vec2DMatrix { data }
+        Vec2DMatrix { data, shape }
     }
 }
 
-impl<T: std::fmt::Display> fmt::Display for Vec2DMatrix<T> {
+impl<T: std::ops::Add + ops::Add<Output = T> + Copy> ops::Add for Vec2DMatrix<T> {
+    type Output = Vec2DMatrix<T>;
+
+    fn add(self, _rhs: Vec2DMatrix<T>) -> Vec2DMatrix<T> {
+        if self.shape != _rhs.shape {
+            panic!("SHAPES MUST BE THE SAME (bad error handling whatever)");
+        }
+
+        let mut data = Vec::new();
+
+        for i in 0..self.data.len() {
+            data.push(Vec::new());
+            for j in 0..self.data[i].len() {
+                data[i].push(self.data[i][j] + _rhs.data[i][j]);
+            }
+        }
+
+        Vec2DMatrix {
+            data,
+            shape: self.shape,
+        }
+    }
+}
+
+impl<T: std::fmt::Display + Copy> fmt::Display for Vec2DMatrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "shape=({}, {})\n", self.shape.0, self.shape.1)?;
+
         let mut i = 0;
         let mut j = self.data[i].len() - 1;
 
@@ -55,7 +84,7 @@ impl<T: std::fmt::Display> fmt::Display for Vec2DMatrix<T> {
             write!(f, "{}, ", self.data[i][a])?;
         }
         write!(f, "{}]]", self.data[i][j])?;
-    
+
         Ok(())
     }
 }
